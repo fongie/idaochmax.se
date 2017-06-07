a6bb90483a72ca2ab1455ebe8283ed11c1020187
Max Körlinge <mkorlinge@hotmail.com>
2017-06-07 16:48:31 +0200

    fixed styling of Home

--- idamaxreact/src/Home.js
+++ idamaxreact/src/Home.js
@@ -1,22 +1,73 @@
 import React, { Component } from 'react';
-import { Button, ButtonGroup } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 
 class Home extends Component {
+    constructor(props) {
+        super(props);
+        this.renderTrips = this.renderTrips.bind(this);
+
+        const trips = [
+            {
+                name: 'Sydamerika 2016', 
+                id: 'sydam'
+            },
+            {
+                name: 'Indonesien 2017',
+                id: 'indo'
+            },
+        ];
+        const generalStyling = {
+            listStyle: 'none',
+            display: 'flex',
+            flexFlow: 'row wrap',
+            justifyContent: 'center',
+            marginBottom: '100px',
+
+        };
+        const thumbnailStyling = {
+            position: 'relative',
+            float: 'left',
+            width: '680px',
+            height: '500px',
+            backgroundPosition: '50% 50%',
+            backgroundRepeat: 'no-repeat',
+            backgroundSize: 'cover',
+            borderRadius: '10%',
+            cursor: 'pointer',
+        };
+        this.state = {
+            generalStyling,
+            thumbnailStyling,
+            trips,
+        }
+    }
+
+    renderTrips() {
+        const toRender = this.state.trips.map(
+            (item, i) => {
+                const tripinfo = require(`../res/${item.id}/tripinfo.json`);
+                const bgImage = require(`../res/${item.id}/img/${tripinfo.bgimage}`);
+                return (
+                    <div key={i} style={{width: '700px', height:'500px', padding: '20px'}}>
+                        <LinkContainer to={`/${item.id}`}>
+                        <li>
+                            <h3>{item.name}</h3>
+                            <div style={{...this.state.thumbnailStyling, backgroundImage: `url(${bgImage})`}}>
+                            </div>
+                        </li>
+                    </LinkContainer>
+                    </div>
+                );
+
+            });
+        return toRender;
+    }
     render() {
         return (
-            <div>
-                <ButtonGroup bsSize="lg">
-                    <LinkContainer to="/sydam">
-                        <Button>Sydamerika</Button>
-                    </LinkContainer>
-                    <LinkContainer to="/indo">
-                        <Button>Indonesien</Button>
-                    </LinkContainer>
-                </ButtonGroup>
-            </div>
+                <div style={{...this.state.generalStyling}}>
+                    { this.renderTrips() }
+                </div>
         );
     }
 }
-
 export default Home;
