b17a6f886d3fe9cb4cddcb6817a5613d9642d208
fongie <max@maxk.se>
2018-08-01 22:32:46 +0200

    updated to 1/8 2018 croatia

--- res/kroatien/blog/convalltxttojson.py
+++ res/kroatien/blog/convalltxttojson.py
@@ -1,4 +1,6 @@
 #coding=UTF-8
 import blogtojson
 
-blogtojson.main("20180729", "Mot Kroatien!", "Max", "DSC_0841.JPG")
+blogtojson.main("20180730", "Trogir och avresa", "Max", "DSC_0846.JPG")
+blogtojson.main("20180731", "Krka", "Max", "20180731_152711.jpg")
+blogtojson.main("20180801", "Vi har spontanbestigit ett berg idag!", "Max", "")
