0670830e81c616d69c32a7e080d88be4c156cb04
fongie <max@maxk.se>
2018-08-07 00:23:15 +0200

    finish croat

--- res/kroatien/blog/convalltxttojson.py
+++ res/kroatien/blog/convalltxttojson.py
@@ -4,3 +4,5 @@ import blogtojson
 blogtojson.main("20180730", "Trogir och avresa", "Max", "DSC_0846.JPG")
 blogtojson.main("20180731", "Krka", "Max", "20180731_152711.jpg")
 blogtojson.main("20180801", "Vi har spontanbestigit ett berg idag!", "Max", "20180801_142651.jpg")
+blogtojson.main("20180803", "Mot Split!", "Max", "20180802_150921.jpg")
+blogtojson.main("20180806", "Ö och Split!", "Max", "20180804_193940.jpg")
