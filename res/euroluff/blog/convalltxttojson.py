d97a5863caa22fde1c0c4314bc38f1679fa57721
fongie <mkorlinge@hotmail.com>
2022-07-03 16:49:38 +0200

    add quick euroluff blog

---
+++ res/euroluff/blog/convalltxttojson.py
@@ -0,0 +1,5 @@
+#coding=UTF-8
+import blogtojson
+
+blogtojson.main("20190706", "Första vandringen i Dolomiterna", "Max", "20190706_114224.jpg")
+blogtojson.main("20190708", "Seeking sector 5", "Ida", "20190708_124945.jpg")
